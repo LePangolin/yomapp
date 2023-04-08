b14e07ede5f2daffeec6c9cfdef2b709ae9381d1
LePangolin <91209500+LePangolin@users.noreply.github.com>
2023-04-08 16:46:44 +0200

    feat(acceuil): template

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'View/acceuil_view.dart';
+import 'Theme/app_theme.dart';
 
 void main() {
- 
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: AppTheme.lightTheme,
+    home: const PageAcceuil(),
+  ));
 }
 
